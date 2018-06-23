b569a86dea91a5d2a1d83f6cda38a9c0c0d1e3eb
Joshua Spence <josh@joshuaspence.com>
2018-06-24 09:21:27 +1000

    Ensure that NodeJS and NPM are installed before attempting to install any NPM packages

--- spec/acceptance/aphlict_spec.rb
+++ spec/acceptance/aphlict_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe 'phabricator::aphlict' do
     # installed. See https://github.com/puppetlabs/puppetlabs-apt/#adding-new-sources-or-ppas.
     Class['apt::update'] -> Package <| title != 'apt-transport-https' and title != 'ca-certificates' and title != 'software-properties-common' |>
 
+    # Ensure that NodeJS and NPM are installed before attempting to install
+    # any NPM packages.
+    Class['nodejs::install'] -> Package <| provider == 'npm' |>
+
     class { 'php::globals':
       php_version => '7.2',
     }
